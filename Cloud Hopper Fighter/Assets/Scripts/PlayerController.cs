1b35c8ba20a9dd9a139c833fd22c1db0d6f35803
hex248 <oliverbryan248@gmail.com>
2022-07-02 22:45:26 +0100

    Basic player movement
    Controller + keyboard (8 directions)

--- Cloud Hopper Fighter/Assets/Scripts/PlayerController.cs
+++ Cloud Hopper Fighter/Assets/Scripts/PlayerController.cs
@@ -24,20 +24,75 @@ enum InputModes
 public class PlayerController : MonoBehaviour
 {
     InputModes inputMode;
-    [SerializeField] bool lockedMovement = false;
+    Rigidbody rb;
 
+    [Header("Movement")]
+    [SerializeField] bool lockedMovement = false;
     [SerializeField] float moveSpeed = 5.0f;
+    [SerializeField][Range(0.0f,1.0f)] float airMultiplier = 0.75f;
+    [SerializeField] [Range(0.0f, 100.0f)] float jumpForce= 50.0f;
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] float groundDrag = 0.5f;
+    [SerializeField] Transform orientation;
+    [SerializeField] float rotationSmoothing = 0.1f;
+    bool grounded;
+    bool canJump;
+
+    Quaternion desiredRotation;
+
+    [Header("Animation")]
+    [SerializeField] Animator animator;
+
+
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        MovementDirections direction = MovementDirections.none;
+        grounded = Physics.Raycast(transform.position, Vector3.down, 0.7f, groundLayer);
 
-        if (inputMode == InputModes.keyboard)
+        if (grounded) rb.drag = groundDrag;
+        else rb.drag = 0;
+
+
+        if (grounded)
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+        }
+
+        Vector3 moveDirection = orientation.forward * Input.GetAxisRaw("Vertical") + orientation.right * Input.GetAxisRaw("Horizontal");
+
+        float speed = grounded ? moveSpeed : moveSpeed * airMultiplier;
+        rb.AddForce(moveDirection.normalized * speed, ForceMode.Force);
+
+        Vector3 flatVelocity = new Vector3(rb.velocity.x, 0.0f, rb.velocity.z);
+
+        if (flatVelocity.magnitude > moveSpeed)
+        {
+            Vector3 limitedVelocity = flatVelocity.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVelocity.x, rb.velocity.y, limitedVelocity.z);
+        }
+
+
+
+        if (moveDirection != Vector3.zero) desiredRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, rotationSmoothing);
+
+        if (Input.GetAxisRaw("Jump") > 0 && canJump) Jump();
+
+        animator.SetBool("moving", moveDirection != Vector3.zero);
+
+        // old movement system
+        #region
+        MovementDirections direction = MovementDirections.none;
+        if (1 > 2 && inputMode == InputModes.keyboard)
         {
             if (lockedMovement)
             {
@@ -101,7 +156,104 @@ public class PlayerController : MonoBehaviour
             {
                 float horizontal = Input.GetAxisRaw("Horizontal");
                 float vertical = Input.GetAxisRaw("Vertical");
+
+                if (vertical == 1.0f) // up
+                {
+                    switch (horizontal)
+                    {
+                        case -1.0f:
+                            direction = MovementDirections.upLeft;
+                            break;
+                        case 1.0f:
+                            direction = MovementDirections.upRight;
+                            break;
+                        case 0.0f:
+                            direction = MovementDirections.up;
+                            break;
+                        default:
+                            direction = MovementDirections.none;
+                            break;
+                    }
+                }
+                else if (vertical == -1.0f) //down
+                {
+                    switch (horizontal)
+                    {
+                        case -1.0f:
+                            direction = MovementDirections.downLeft;
+                            break;
+                        case 1.0f:
+                            direction = MovementDirections.downRight;
+                            break;
+                        case 0.0f:
+                            direction = MovementDirections.down;
+                            break;
+                        default:
+                            direction = MovementDirections.none;
+                            break;
+                    }
+                }
+                else if (vertical == 0)
+                {
+                    switch (horizontal)
+                    {
+                        case -1.0f:
+                            direction = MovementDirections.left;
+                            break;
+                        case 1.0f:
+                            direction = MovementDirections.right;
+                            break;
+                        case 0.0f:
+                            direction = MovementDirections.none;
+                            break;
+                        default:
+                            direction = MovementDirections.none;
+                            break;
+                    }
+                }
+
+                Debug.Log(direction);
+
+                Vector3 movement;
+                switch (direction)
+                {
+                    case MovementDirections.up:
+                        movement = new Vector3(1.0f, 0.0f, 1.0f);
+                        break;
+                    case MovementDirections.down:
+                        movement = new Vector3(-1.0f, 0.0f, -1.0f);
+                        break;
+                    case MovementDirections.left:
+                        movement = new Vector3(-1.0f, 0.0f, 1.0f);
+                        break;
+                    case MovementDirections.right:
+                        movement = new Vector3(1.0f, 0.0f, -1.0f);
+                        break;
+                    case MovementDirections.upLeft:
+                        movement = new Vector3(0.0f, 0.0f, 1.0f);
+                        break;
+                    case MovementDirections.upRight:
+                        movement = new Vector3(1.0f, 0.0f, 0.0f);
+                        break;
+                    case MovementDirections.downLeft:
+                        movement = new Vector3(-1.0f, 0.0f, 0.0f);
+                        break;
+                    case MovementDirections.downRight:
+                        movement = new Vector3(0.0f, 0.0f, -1.0f);
+                        break;
+                    default:
+                        movement = Vector3.zero;
+                        break;
+                }
+
+                rb.AddForce(movement.normalized * moveSpeed * 10f, ForceMode.Force);
             }
         }
+        #endregion
+    }
+
+    void Jump()
+    {
+        rb.AddForce(0, jumpForce, 0);
     }
 }
